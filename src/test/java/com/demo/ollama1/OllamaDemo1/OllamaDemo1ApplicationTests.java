6693a20cd79acac9f8cc570a88ba9458b3aa8041
Vivid-Vortex <ctntc9.dpk@gmail.com>
2025-03-15 15:38:51 +0530

    init commit

---
+++ src/test/java/com/demo/ollama1/OllamaDemo1/OllamaDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.ollama1.OllamaDemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllamaDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
